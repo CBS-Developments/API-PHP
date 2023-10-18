e6d3f7b9c9aeea6efaf82974846e24f0770375ac
Shahiru Edirisinghe <shahiru.edirisinghe@gmail.com>
2023-10-18 10:45:17 +0530

    Updated

--- editSubTask.php
+++ editSubTask.php
@@ -5,10 +5,8 @@ require_once("dbconfig.php");
 
 $task_id  = $_POST['task_id'];
 $task_title  = $_POST['task_title'];
-$task_type  = $_POST['task_type'];
 $task_type_name  = $_POST['task_type_name'];
 $task_description  = $_POST['task_description'];
-$task_status  = $_POST['task_status'];
 $task_status_name = $_POST['task_status_name'];
 $action_taken_by_id = $_POST['action_taken_by_id'];
 $action_taken_by = $_POST['action_taken_by'];
@@ -18,19 +16,23 @@ $task_edit_by = $_POST['task_edit_by'];
 $task_edit_by_id = $_POST['task_edit_by_id'];
 $task_edit_by_date = $_POST['task_edit_by_date'];
 $task_edit_by_timestamp = $_POST['task_edit_by_timestamp'];
+$company  = $_POST['company'];
+$due_date  = $_POST['due_date'];
+$assign_to  = $_POST['assign_to'];
+$source_from  = $_POST['source_from'];
+$category_name  = $_POST['category_name'];
+$category  = $_POST['category'];
 
-$query = "SELECT * FROM task WHERE task_id='$task_id'";  
+$query = "SELECT * FROM sub_task WHERE task_id='$task_id'";  
  
 $res = mysqli_query($con,$query);
 $data = mysqli_fetch_array($res);
 if ($data[0] >= 1) {
-        $query = "UPDATE task SET  
+        $query = "UPDATE sub_task SET  
 
 task_title = '$task_title',  
-task_type = '$task_type',  
 task_type_name = '$task_type_name',  
 task_description = '$task_description',  
-task_status = '$task_status',
 task_status_name = '$task_status_name',
 action_taken_by_id = '$action_taken_by_id',
 action_taken_by = '$action_taken_by',
@@ -40,6 +42,12 @@ task_edit_by = '$task_edit_by',
 task_edit_by_id = '$task_edit_by_id',
 task_edit_by_date = '$task_edit_by_date',
 task_edit_by_timestamp = '$task_edit_by_timestamp',
+company = '$company',
+due_date = '$due_date',
+assign_to = '$assign_to',
+source_from = '$source_from',
+category_name = '$category_name',
+category = '$category'
 
 WHERE task_id='$task_id'";
     $res = mysqli_query($con, $query);
